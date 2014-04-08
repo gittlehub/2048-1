0d3800715588b99950ad8c48ab81ddf0aac43231
Jijin John Manalikkal <jijinjohn@gmail.com>
2014-04-08 14:03:01 +0530

    Added function to move left.

---
+++ moves.c
@@ -0,0 +1,31 @@
+#include <moves.h>
+
+#define INROW (row, pos) (row == pos/4)
+#define INCOL (col, pos) (col == pos%4)
+
+void left (int *grid)
+{
+	int row;
+	int col;
+	int prev;
+	int current;
+	int next;
+	for (row = 0; row < 4; row++) {
+		for (col = 0; col < 4; col++) {
+			current = row*4 + col;
+			prev = row*4 + col - 1;
+			next = row*4 + col + 1;
+			/* Merge tile on the right if equal */
+			if (INROW(row, next) && (grid[current] == grid[next])) {
+				grid[current] += grid[next];
+				grid[next] = 0;
+			}
+			/* Check if this can be shifted */
+			while (INROW(row, prev) && (grid[prev] == 0)) {
+				grid[prev--] = grid[current];
+				grid[current--] = 0;
+			}
+		}
+	}
+}
+
